27fbf7556a54c536bf4b709d3acf5410d82cbe3d
AnikKDev <anikdev2016@gmail.com>
2022-04-29 15:39:09 +0600

    completed get an item by id API

--- index.js
+++ index.js
@@ -2,10 +2,45 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hekzg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db("maniadb").collection("items");
+
+        // get all available items from inventory
+        app.get('/items', async (req, res) => {
+            // search all items
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // get a item by searching its id
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemCollection.findOne(query);
+            res.send(result);
+        })
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
