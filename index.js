e1254bad35707f8a6c0c42bbbb91ad1c1e11407a
AnikKDev <anikdev2016@gmail.com>
2022-05-03 19:01:10 +0600

    completed jwt implementation completely

--- index.js
+++ index.js
@@ -10,7 +10,23 @@ const ObjectId = require('mongodb').ObjectId;
 app.use(cors());
 app.use(express.json());
 
+// jwt function
+const verifyToken = (req, res, next) => {
+    const headerAuth = req.headers.authorization;
+    if (!headerAuth) {
+        return res.status(401).send({ message: 'Unauthorized' })
+    }
+    const token = headerAuth.split(' ')[1];
+    jwt.verify(token, process.env.JWT_TOKEN, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ message: 'Forbidden request' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
 
+}
 
 
 
@@ -25,7 +41,7 @@ async function run() {
         app.post('/token', async (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.JWT_TOKEN);
-            res.send(token);
+            res.send({ token });
         })
         // get six items from inventory
         app.get('/inventory', async (req, res) => {
@@ -90,13 +106,19 @@ async function run() {
 
 
         // get my items from inventory
-        app.get('/my-items', async (req, res) => {
-            const email = req.query.email;
-            // console.log(email);
-            const query = { email: email };
-            const cursor = itemCollection.find(query);
-            const items = await cursor.toArray();
-            res.send(items);
+        app.get('/my-items', verifyToken, async (req, res) => {
+            const decodedData = req?.decoded?.email;
+            // console.log(decodedData);
+            const email = req?.query?.email;
+            if (email === decodedData) {
+                // console.log(email);
+                const query = { email: email };
+                const cursor = itemCollection.find(query);
+                const items = await cursor.toArray();
+                res.send(items);
+            } else {
+                res.status(403).send({ message: 'Forbidden request' })
+            }
         })
 
 
